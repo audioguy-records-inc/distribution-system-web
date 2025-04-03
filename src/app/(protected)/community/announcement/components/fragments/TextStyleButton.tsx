89e82774b2af3558e4a5d32d2930d111dd00c7b0
Saehanseul <saehanseul@gmail.com>
2025-04-03 17:02:13 +0900

    feat: 커뮤니티 추가중

---
+++ src/app/(protected)/community/announcement/components/fragments/TextStyleButton.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  padding: 12px 32px;
+`;
+
+const TextStyleButton = () => {
+  return <Container></Container>;
+};
+
+export default TextStyleButton;
