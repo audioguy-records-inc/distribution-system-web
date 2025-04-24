8403dfc7dfdfc635e8e4488d3d53c435f91e6785
Saehanseul <saehanseul@gmail.com>
2025-04-24 15:11:00 +0900

    bug: 가사 등록 버그 수정

--- src/app/(protected)/content/album/new/components/fragment/LyricsAddModal.tsx
+++ src/app/(protected)/content/album/new/components/fragment/LyricsAddModal.tsx
@@ -66,7 +66,7 @@ export default function LyricsAddModal({
   };
 
   const handleSubmit = async () => {
-    if (!track || !track._id) return;
+    if (!track) return;
 
     setIsLoading(true);
     try {
